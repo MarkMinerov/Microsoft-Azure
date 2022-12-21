66a5bc20695860db2eb76e77aed449c492f0863d
Mark Minerov <markminerov123@gmail.com>
2022-12-21 22:08:17 +0100

    css updates

--- index.php
+++ index.php
@@ -4,40 +4,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>IoT Lock</title>
-  <style>
-    input[type="text"] {
-      width: 15rem;
-      border: 1px solid orange;
-    }
-
-    li {
-      margin-bottom: 0.5rem;
-    }
-
-    li span {
-      margin-right: 1rem;
-    }
-  </style>
 </head>
 <body>
-  <h2>IoT Admin Panel</h2>
-  <form id="pinForm">
-    <input id="prevPinInput" type="text" placeholder="Enter previous pin" required>
-    <br><br>
-    <input id="newPinInput" type="text" placeholder="Enter new pin" required>
-    <br><br>
-    <input type="submit" value="Change pin">
-    <br><br><br>
-  </form>
+  <div class="container">
+    <h2>IoT Admin Panel</h2>
+    <form id="pinForm">
+      <input id="prevPinInput" type="text" placeholder="Enter previous pin" required>
+      <input id="newPinInput" type="text" placeholder="Enter new pin" required>
+      <input type="submit" value="Change pin">
+    </form>
 
-  <form id="addChipForm">
-    <input type="text" placeholder="Enter chip id to add new one..." id="addChipInput" required>
-    <br><br>
-    <input type="submit" value="Add new chip">
-  </form>
+    <form id="addChipForm">
+      <input type="text" placeholder="Enter chip id to add new one..." id="addChipInput" required>
+      <input type="submit" value="Add new chip">
+    </form>
 
-  <ul id="chipsList"></ul>
+    <ul id="chipsList"></ul>
+    </div>
 
   <script defer src="main.js"></script>
 </body>
